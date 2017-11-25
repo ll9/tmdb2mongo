2298f1f5ec7a1480b4b4f31c06671a301458e160
ll9 <llwierer@web.de>
2017-11-25 11:44:37 +0100

    fixed delay issue

--- helper.js
+++ helper.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var rp = require('request-promise');
 
 var getMax = function (api_key) {
     return new Promise(function(resolve, reject) {
@@ -15,5 +16,24 @@ var getMax = function (api_key) {
     });
 };
 
+var getMovieData = (api_key, id) => {
+    return new Promise((resolve, reject) => {
+        rp("https://api.themoviedb.org/3/movie/" + id + "?api_key=" + api_key + "&append_to_response=videos")
+            .then((html) => {
+                data = JSON.parse(html);
+                if (data.status_code != 34) {
+                    resolve(data);
+                }
+            })
+            .catch((error) => {
+                reject(error.statusCode);
+                if (error.statusCode === 429) { // made over 40 requests in 10 seconds
+                    throw "Too many requests within 10 seconds (40 allowed)";
+                }
+            });
+    });
+};
+
 
 module.exports.getMax = getMax;
+module.exports.getMovieData = getMovieData;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var rp = require('request-promise');
 var MongoClient = require('mongodb').MongoClient;
 var api_key = require('./config.json').key;
 var helper = require('./helper.js');
@@ -11,37 +12,39 @@ helper.getMax(api_key)
         if (err) {
             throw err;
         }
+
         console.log("Started at: " + new Date().toISOString());
         console.log("This application will log information whenever 1000 movies were processed");
         console.log("Still ned " + MAX + " Movies to process, estimated time: ~" + MAX/240 + " minutes\n");
         var insertions = 0;
-        (function myLoop (i) {          
-            setTimeout(function () {   
-        
-            var url = "https://api.themoviedb.org/3/movie/" + i + "?api_key=" + api_key + "&append_to_response=videos";
-                request(url, function (error, response, body) {
-                    if (error) {
-                        console.log('Lost internet connection, last successful id was: ' + (i-1));
-                        console.log('Retrying in 10 seconds');
-                        setTimeout(function() {myLoop(i)}, 10000);
+        var i = 0;
+
+        var interval = setInterval(() => {
+            i++
+            
+            helper.getMovieData(api_key, i)
+                .then((data) => {
+                    db.collection('tmdb').insertOne(data);
+                    insertions++;
+                })
+                .catch((statusCode) => {
+                    if (statusCode === 429) {
+                        console.log("Accessed over 40 files within 10 seconds");
+                        process.exit();
                     }
-                    else {
-                        var count = db.collection('tmdb').find().count();
-                        var data = JSON.parse(body);
-                        if (data.status_code != 34) { //Code 34 means that this resource doesn't exist anymore at this location (removed/eddited)
-                            db.collection('tmdb').insertOne(JSON.parse(body));
-                            insertions++;
-                            if (insertions % 1000 == 0) {
-                                console.log(new Date().toISOString());
-                                console.log("inserted " + insertions + " movies, " + "processed " + i + " ID'S still " + (MAX - i) + " to go, estimated time: ~" + (MAX-i)/240 + " minutes\n");
-                            }
-                        }
-                        if (++i < MAX) 
-                            myLoop(i);      //  increment i and call myLoop again if i < MAX
+                })
+                .then(() => {
+                    if (i % 40 == 0) {
+                        console.log(new Date().toISOString());
+                        console.log("inserted " + insertions + " movies, " + "processed " + i + " ID'S still " + (MAX - i) + " to go, estimated time: ~" + (MAX-i)/240 + " minutes\n");
                     }
-                });                        
-        }, 250)
-        })(0); 
+                })
+                
+            if (i >= MAX) {
+                console.log("Finished");
+                clearInterval(interval);
+            }
+        }, 260)
     });
 });
 
